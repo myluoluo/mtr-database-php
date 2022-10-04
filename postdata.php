904859c36d6b2670d1c8e16df4ff8cb628e7f1fd
myluoluo <admin@myluoluo.com>
2022-10-05 00:55:56 +0800

    Update postdata.php

--- postdata.php
+++ postdata.php
@@ -46,31 +46,40 @@ if (!isset($data['report'])) {
     die("Error!: MTR output result is wrong");
 }
 
+$asPath = [];
+foreach ($data['report']['hubs'] as $hub) {
+    if(!isset($hub['ASN']) || $hub['ASN'] == 'AS???') continue;
+    if(end($asPath) == $hub['ASN']) continue;
+    $asPath[] = $hub['ASN'];
+}
+$asPath = implode(' -> ', $asPath);
+
 // Get end hub
 $endHub = end($data['report']['hubs']);
 // Save to database
 $insertMap = [
-    'start_datetime' => $startDateTime,
-    'end_datetime' => $endDateTime,
-    'category' => $category,
-    // 'source' => $data['report']['mtr']['src'],
-    'source' => $hostname,
-    'destination' => $data['report']['mtr']['dst'],
-    'interval' => $interval,
-    'host' => $endHub['host'],
-    'asn' => $endHub['ASN'],
-    'mtr_loss' => $endHub['Loss%'],
-    'mtr_sent' => $endHub['Snt'],
-    'mtr_avg' => $endHub['Avg'],
-    'mtr_best' => $endHub['Best'],
-    'mtr_worst' => $endHub['Wrst'],
-    'mtr_raw' => $output,
-    'command' => $cmd,
+    'start_datetime'    => $startDateTime,
+    'end_datetime'      => $endDateTime,
+    'category'          => $category,
+    // 'source'         => $data['report']['mtr']['src'],
+    'source'            => $hostname,
+    'destination'       => $data['report']['mtr']['dst'],
+    'interval'          => $interval,
+    'host'              => $endHub['host'],
+    'asn'               => $endHub['ASN'],
+    'as_path'           => $asPath,
+    'mtr_loss'          => $endHub['Loss%'],
+    'mtr_sent'          => $endHub['Snt'],
+    'mtr_avg'           => $endHub['Avg'],
+    'mtr_best'          => $endHub['Best'],
+    'mtr_worst'         => $endHub['Wrst'],
+    'mtr_raw'           => $output,
+    'command'           => $cmd,
 ];
 
-$sql = 'INSERT INTO ' . $dbConfig['table'] . ' (`sn`, `start_datetime`, `end_datetime`, `interval`, `category`, `source`, `destination`, `host`, `asn`, `mtr_loss`, `mtr_sent`, `mtr_avg`, `mtr_best`, `mtr_worst`, `mtr_raw`, `command`) ';
+$sql = 'INSERT INTO ' . $dbConfig['table'] . ' (`sn`, `start_datetime`, `end_datetime`, `interval`, `category`, `source`, `destination`, `host`, `asn`, `as_path`, `mtr_loss`, `mtr_sent`, `mtr_avg`, `mtr_best`, `mtr_worst`, `mtr_raw`, `command`) ';
 // $sql = "INSERT INTO {$dbConfig['table']} (sn, start_datetime, end_datetime, interval, category, source, destination, host, asn, mtr_loss, mtr_sent, mtr_avg, mtr_best, mtr_worst, mtr_raw, command) 
-$sql .= "VALUES (NULL, :start_datetime, :end_datetime, :interval, :category, :source, :destination, :host, :asn, :mtr_loss, :mtr_sent, :mtr_avg, :mtr_best, :mtr_worst, :mtr_raw, :command)";
+$sql .= "VALUES (NULL, :start_datetime, :end_datetime, :interval, :category, :source, :destination, :host, :asn, :as_path, :mtr_loss, :mtr_sent, :mtr_avg, :mtr_best, :mtr_worst, :mtr_raw, :command)";
 $stmt = $conn->prepare($sql);
 foreach ($insertMap as $key => $value) {
     $stmt->bindValue(":{$key}", $value);
